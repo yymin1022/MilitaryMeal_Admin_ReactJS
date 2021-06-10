4e12bc7feca0e5fdfcc9fd18c28274813dc0a3b6
yymin1022 <yymin1022@gmail.com>
2021-06-10 04:52:57 +0000

    Update App.js : Add Reouter Layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Switch} from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import DefaultLayout from "./layout/Default";
+import RouteLayout from "./layout/RouteLayout";
+
+import LoginView from "./route/MainView";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <RouteLayout
+          exact
+          path="/"
+          layout={DefaultLayout}
+          component={MainView} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
