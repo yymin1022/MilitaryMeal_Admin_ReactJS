9b0ebe1637c2c53a5e6f01fe2f1fa4f27af755d9
yymin1022 <yymin1022@gmail.com>
2021-06-10 10:26:28 +0000

    UPdate Default.js : Fix Importing Error

--- src/layout/Default/Default.js
+++ src/layout/Default/Default.js
@@ -2,8 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 
-import "./Default.css";
-
 const Default = props => {
   const{
     className,
