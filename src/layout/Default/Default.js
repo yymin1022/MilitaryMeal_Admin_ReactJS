d7b3ff5ff1072cbe83627fe742e0379e70ab0d13
yymin1022 <yymin1022@gmail.com>
2021-06-10 05:13:42 +0000

    Update Default.js : Add Default Layout

---
+++ src/layout/Default/Default.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
+
+import "./Default.css";
+
+const Default = props => {
+  const{
+    className,
+    children,
+  } = props;
+  
+  return(
+    <div className={className}>
+      <div className="Header">
+        <div className="HeaderLogo">
+          LOGO
+        </div>
+        <div className="HeaderNav">
+          <Link to="/">
+            <div>Home</div>
+          </Link>
+        </div>
+      </div>
+      <div>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+Default.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+}
+
+export default Default;
