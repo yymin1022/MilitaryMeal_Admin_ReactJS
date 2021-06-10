32afb5e8593ced2b455d997217c7b42c6f2ee88e
yymin1022 <yymin1022@gmail.com>
2021-06-10 05:15:39 +0000

    Update MainView.js : Add Default Layout

---
+++ src/route/MainView/MainView.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class MealView extends Component{
+    constructor(){
+        super();
+        this.state = {};
+    }
+
+    render(){
+        return(
+            <div className="MainView">
+                Main Layout
+            </div>
+        );
+    }
+}
+ 
+export default MealView;
