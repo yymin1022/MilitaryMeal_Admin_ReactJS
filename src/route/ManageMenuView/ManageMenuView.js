9812810de5ce47879e5665b29f7b398bb52e27b6
yymin1022 <yymin1022@gmail.com>
2021-06-13 13:42:34 +0000

    Cleaned UP

--- src/route/ManageMenuView/ManageMenuView.js
+++ src/route/ManageMenuView/ManageMenuView.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React from "react";
 
-import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 import "./ManageMenuView.css";
 
