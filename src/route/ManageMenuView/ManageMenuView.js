0013b4205235948e71a9c6a614bcfdfcfc03f45a
yymin1022 <yymin1022@gmail.com>
2021-06-13 13:22:54 +0000

    Update Files : Remove react-autocomplete-input Library

--- src/route/ManageMenuView/ManageMenuView.js
+++ src/route/ManageMenuView/ManageMenuView.js
@@ -1,21 +1,11 @@
 import React from 'react';
 
-import TextInput from 'react-autocomplete-input';
-
 import "./ManageMenuView.css";
-import 'react-autocomplete-input/dist/bundle.css';
 
 const ManageMenuView = () => {
     return(
         <div>
             ManageMenu Layout
-
-            <TextInput
-                options={{
-                    "": ["PIZZA", "SPAGHETTI", "돼지고기 김치볶음", "돼지고기 청경채볶음", "된장찌개", "돼지고기 김치찌개", "햄소시지찌개", "쌀밥", "현미밥"]
-                }}
-                matchAny={true}
-                trigger={[""]} />
         </div>
     );
 }
