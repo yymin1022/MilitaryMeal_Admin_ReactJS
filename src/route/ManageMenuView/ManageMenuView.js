e32ba41bcffc1647e745c6679980d0b063c1762e
yymin1022 <yymin1022@gmail.com>
2021-06-13 11:39:10 +0000

    Update ManageMenuView.js : Add AUTO-COMPLETE Text Input Component

--- src/route/ManageMenuView/ManageMenuView.js
+++ src/route/ManageMenuView/ManageMenuView.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import TextInput from 'react-autocomplete-input';
+
 import "./ManageMenuView.css";
+import 'react-autocomplete-input/dist/bundle.css';
 
 const ManageMenuView = () => {
     return(
         <div>
             ManageMenu Layout
+            <TextField trigger={["@", "@@"]} options={{"@": ["aa", "ab", "abc", "abcd"], "@@": ["az", "ar"]}} />
         </div>
     );
 }
