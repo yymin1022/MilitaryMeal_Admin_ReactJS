cdd2f2eaa207650a301edc1e6336b2f5d9864393
yymin1022 <yymin1022@gmail.com>
2021-06-13 13:31:59 +0000

    Update ManageMenuView.js : Add Autocomplete ComboBox Component

--- src/route/ManageMenuView/ManageMenuView.js
+++ src/route/ManageMenuView/ManageMenuView.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
 import "./ManageMenuView.css";
 
 const ManageMenuView = () => {
+    const menuList = [
+        {menu: "돼지고기 김치볶음"},
+        {menu: "돼지고기 청경채볶음"},
+        {menu: "김치찌개"},
+        {menu: "된장찌개"},
+        {menu: "호박된장찌개"},
+        {menu: "배추김치"},
+        {menu: "열무김치"},
+        {menu: "쌀밥"},
+        {menu: "현미밥"}
+    ]
+
     return(
         <div>
             ManageMenu Layout
+
+            <Autocomplete
+                options={menuList}
+                getOptionLabel={(option) => 
+                    option.menu}
+                style={{width: 300}}
+                renderInput={(params) => <TextField
+                    {...params}
+                    label="Input Menu Name"
+                    variant="outlined" />
+                }/>
         </div>
     );
 }
