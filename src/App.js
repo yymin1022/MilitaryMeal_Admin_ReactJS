41dc7a69359a8c2983bc346ea2290e5fec00efbb
yymin1022 <yymin1022@gmail.com>
2021-06-13 04:28:49 +0000

    Update App.js : Add Routing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import {BrowserRouter as Router, Switch} from "react-router-dom";
 import DefaultLayout from "./layout/Default";
 import RouteLayout from "./layout/RouteLayout";
 
+import ApproveUserView from "./route/ApproveUserView";
 import MainView from "./route/MainView";
+import ManageMenuView from "./route/ManageMenuView";
+import RankView from "./route/RankView";
 // import axios from "axios";
 
 const App = () => {
@@ -24,6 +27,21 @@ const App = () => {
                     path="/"
                     layout={DefaultLayout}
                     component={MainView} />
+                <RouteLayout
+                    exact
+                    path="/approve"
+                    layout={DefaultLayout}
+                    component={ApproveUserView} />
+                <RouteLayout
+                    exact
+                    path="/manage"
+                    layout={DefaultLayout}
+                    component={ManageMenuView} />
+                <RouteLayout
+                    exact
+                    path="/rank"
+                    layout={DefaultLayout}
+                    component={RankView} />
             </Switch>
         </Router>
     );
