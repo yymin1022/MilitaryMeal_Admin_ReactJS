cc7e000a77c8d2d42f44ffc8b2911160f6e2030e
yymin1022 <yymin1022@gmail.com>
2021-06-13 01:36:31 +0000

    Update Default.js : Apply CSS

--- src/layout/Default/Default.js
+++ src/layout/Default/Default.js
@@ -2,6 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 
+import "./Default.css";
+
 const Default = props => {
   const{
     className,
